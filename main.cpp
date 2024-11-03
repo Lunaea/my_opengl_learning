d61a8df2e2c4489c8c12f3a3076584ea9c14c662
Lunaea <132797043+Lunaea@users.noreply.github.com>
2024-11-03 09:47:34 -0800

    Link Vertex Attributes

--- main.cpp
+++ main.cpp
@@ -101,6 +101,9 @@ int main(int, char**){
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
     while(!glfwWindowShouldClose(window))
     {
         processInput(window);
